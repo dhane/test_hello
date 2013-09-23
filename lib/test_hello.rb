d7cf48628d16ae9c38ad7cc4426fafb25f876d46
t_ramu <dhanegm731@gmail.com>
2013-09-24 04:05:08 +0900

    use thor

--- lib/test_hello.rb
+++ lib/test_hello.rb
@@ -1,9 +1,11 @@
 require "test_hello/version"
+require "thor"
 
 module TestHello
-  class Command
-    def self.run
-      p 'Hello World !!!'
+  class Command < Thor
+    desc "show", "Show Hello NAME on CLI."
+    def show(name)
+      p "Hello #{name} !!!"
     end
   end
 end
